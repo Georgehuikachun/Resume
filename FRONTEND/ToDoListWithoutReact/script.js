b96fdcc374d7da46fe5142a93c8061275ee43c40
georgee <14868558+georgehuikachun@user.noreply.gitee.com>
2025-09-05 18:53:30 +0930

    finished prototype of todolist

---
+++ FRONTEND/ToDoListWithoutReact/script.js
@@ -0,0 +1,23 @@
+const addBtn=document.querySelector("#addBtn")
+const taskInput=document.querySelector("#taskInput")
+const taskList=document.querySelector("#taskList")
+
+addBtn.addEventListener("click", ()=>{
+    const taskText= taskInput.value.trim();
+    if (taskText==='') return;
+
+    //Create <li>
+    const li=document.createElement("li");
+    li.innerHTML=`${taskText} <button class="delete-btn">Delete</button>`;
+
+    //Delete btn fcn
+    li.querySelector(".delete-btn").addEventListener("click", () =>{
+        li.remove();
+    })
+
+    //add to list
+    taskList.appendChild(li);
+
+    //clear input
+    taskInput.value="";
+});
